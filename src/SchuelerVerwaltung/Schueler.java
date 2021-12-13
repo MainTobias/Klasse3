77278f0117758049a1e9af5ab69a87fc80ca2ce4
MainTobias <61535922+MainTobias@users.noreply.github.com>
2021-12-13 11:17:52 +0100

    ABCDEFGHIJKLMOPQRSTUVWXYZ

---
+++ src/SchuelerVerwaltung/Schueler.java
@@ -0,0 +1,55 @@
+package SchuelerVerwaltung;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public record Schueler(String klasse, String name, String vorname, char geschlecht,
+                       LocalDate geboren, String religion) implements Comparable<Schueler> {
+    final private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public static Schueler makeSchueler(String s) {
+        String[] strings = s.split(";");
+        return new Schueler(strings[0], strings[1], strings[2], strings[3].charAt(0), LocalDate.parse(strings[4], formatter), strings[5]);
+    }
+
+    @Override
+    public int compareTo(Schueler o) {
+        int ret = klasse.compareTo(o.klasse);
+        if (ret == 0) {
+            ret = name.compareTo(o.name);
+        }
+        if (ret == 0) {
+            ret = vorname.compareTo(o.vorname);
+        }
+        if (ret == 0) {
+            ret = geboren.compareTo(o.geboren);
+        }
+        return ret;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Schueler schueler = (Schueler) o;
+        return Objects.equals(klasse, schueler.klasse) && Objects.equals(name, schueler.name) && Objects.equals(vorname, schueler.vorname) && Objects.equals(geboren, schueler.geboren);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(klasse, name, vorname, geboren);
+    }
+
+    @Override
+    public String toString() {
+        return "Schueler{" +
+                "klasse='" + klasse + '\'' +
+                ", name='" + name + '\'' +
+                ", vorname='" + vorname + '\'' +
+                ", geschlecht=" + geschlecht +
+                ", geboren=" + geboren +
+                ", religion='" + religion + '\'' +
+                '}';
+    }
+}
