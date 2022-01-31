bec85dcc8d68afd3cd1529b4eaf99a507f7c33b8
MainTobias <61535922+MainTobias@users.noreply.github.com>
2022-01-31 11:19:00 +0100

    Hotel test not working

--- src/BinaryFiles/CheckInt.java
+++ src/BinaryFiles/CheckInt.java
@@ -12,8 +12,7 @@ public class CheckInt {
 
     public static void createFile(String filename, double... values) throws IOException {
         Path file = Path.of(filename).toAbsolutePath();
-        if (Files.deleteIfExists(file))
-        Files.createFile(file);
+        if (Files.deleteIfExists(file)) Files.createFile(file);
         try (DataOutputStream dos = new DataOutputStream(new FileOutputStream(file.toString()))) {
             dos.writeInt(values.length);
             for (double d : values) {
@@ -23,16 +22,16 @@ public class CheckInt {
     }
 
     public static boolean isValidFile(String filename) throws IOException {
-        try(RandomAccessFile rf = new RandomAccessFile(filename, "r")) {
+        try (RandomAccessFile rf = new RandomAccessFile(filename, "r")) {
             rf.seek(0);
             int len = rf.readInt();
-            return len == (rf.length() - Integer.BYTES) / (double)Double.BYTES;
+            return len == (rf.length() - Integer.BYTES) / (double) Double.BYTES;
         }
     }
 
     public static String getFileInfo(String filename) throws IOException {
         if (!isValidFile(filename)) return "invalid";
-        try(DataInputStream din = new DataInputStream(new FileInputStream(filename))) {
+        try (DataInputStream din = new DataInputStream(new FileInputStream(filename))) {
             StringBuilder sb = new StringBuilder("Saved values: ");
             int len = din.readInt();
             sb.append(len).append("\n");
@@ -46,7 +45,7 @@ public class CheckInt {
 
     public static void append(String filename, double toAppend) throws IOException {
         if (!isValidFile(filename)) throw new IllegalArgumentException(filename + " is malformed");
-        try(RandomAccessFile rf = new RandomAccessFile(filename, "rw")) {
+        try (RandomAccessFile rf = new RandomAccessFile(filename, "rw")) {
             rf.seek(0);
             int x = rf.readInt();
             rf.seek(0);
