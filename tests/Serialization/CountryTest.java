2ee351450f1f0da30134ae9b0effca9a689eb836
MainTobias <61535922+MainTobias@users.noreply.github.com>
2022-02-28 11:21:01 +0100

    DeleteLines.java working

--- tests/Serialization/CountryTest.java
+++ tests/Serialization/CountryTest.java
@@ -17,7 +17,7 @@ class CountryTest {
     @Test
     void serializeAndDeserialize() throws IOException, ClassNotFoundException {
         List<Country> countries = Country.fromCsv(Path.of("resources/Serialization/countries.csv"));
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream(256);
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
         ObjectOutputStream out = new ObjectOutputStream(byteOut);
         out.writeObject(countries);
         out.close();
@@ -31,13 +31,14 @@ class CountryTest {
     void serializeAndDeserializeWithCapitalLetters() throws IOException, ClassNotFoundException {
         List<Country> countries = Country.fromCsv(Path.of("resources/Serialization/countries.csv"));
         Country.setCapitalize(true);
-        ByteArrayOutputStream byteOut = new ByteArrayOutputStream(256);
+        ByteArrayOutputStream byteOut = new ByteArrayOutputStream();
         ObjectOutputStream out = new ObjectOutputStream(byteOut);
         out.writeObject(countries);
         out.close();
         ByteArrayInputStream byteIn = new ByteArrayInputStream(byteOut.toByteArray());
         ObjectInputStream in = new ObjectInputStream(byteIn);
         List<Country> deserializedCountries = (List<Country>) in.readObject();
+        System.out.println(countries);
         assertEquals(countries, deserializedCountries);
     }
 }
