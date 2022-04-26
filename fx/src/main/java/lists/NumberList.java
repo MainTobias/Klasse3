a2c23eea9c01d057d3cf887028463309cfa84e08
tobias <61535922+MainTobias@users.noreply.github.com>
2022-04-26 21:28:19 +0200

    kein fehler mehr bei NumberList.java

--- fx/src/main/java/lists/NumberList.java
+++ fx/src/main/java/lists/NumberList.java
@@ -5,11 +5,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.IntStream;
 
 import static com.Utils.getPath;
@@ -17,30 +21,59 @@ import static com.Utils.getPath;
 public class NumberList extends Application {
 
     @FXML
+    private ToggleGroup selection;
+    @FXML
     private ListView<Integer> left;
     @FXML
     private ListView<Integer> right;
-
     @FXML
-    private ToggleGroup selection;
+    private RadioButton single;
+    @FXML
+    private RadioButton multi;
 
-    private void move(ListView<Integer> from, ListView<Integer> to) {
-        for (Iterator<Integer> it = from.getSelectionModel().getSelectedItems().iterator(); it.hasNext(); ) {
-            Integer i = it.next();
-            to.getItems().add(i);
-            from.getItems().remove(i);
-        }
+    private SelectionMode getSelectionMode() {
+        return single.isSelected() ? SelectionMode.SINGLE : SelectionMode.MULTIPLE;
+    }
+
+
+    private void moveMultiple(ListView<Integer> from, ListView<Integer> to) {
+        List<Integer> l = new ArrayList<>(from.getSelectionModel().getSelectedItems());
+        to.getItems().addAll(l);
+        from.getItems().removeAll(l);
+        Collections.sort(to.getItems());
+    }
+
+    private void moveSingle(ListView<Integer> from, ListView<Integer> to) {
+        Integer i = from.getSelectionModel().getSelectedItem();
+        if (i < 0) return;
+        to.getItems().add(-(Collections.binarySearch(to.getItems(), i)+1), i);
     }
 
     @FXML
     private void moveLeft() {
-        System.out.println(left);
-        move(right, left);
+        if (getSelectionMode() == SelectionMode.SINGLE) {
+            moveSingle(right, left);
+        } else {
+            moveMultiple(right, left);
+        }
     }
 
     @FXML
     private void moveRight() {
-        move(left, right);
+        if (getSelectionMode() == SelectionMode.SINGLE) {
+            moveSingle(left, right);
+        } else {
+            moveMultiple(left, right);
+        }
+    }
+
+    @FXML
+    private void switchSelection() {
+        if (single.isSelected()) {
+            left.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        } else if (multi.isSelected()) {
+            left.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        }
     }
 
 
@@ -54,11 +87,15 @@ public class NumberList extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle(this.getClass().getSimpleName());
         stage.setScene(scene);
-        stage.setOnShown((e) -> System.out.println(left));
         stage.show();
     }
 
-    private void startLogic(Stage stage) {
+
+    public void initialize() {
+        startLogic();
+    }
+
+    private void startLogic() {
         left.getItems().addAll(IntStream.rangeClosed(0, 100).boxed().toList());
     }
 }
